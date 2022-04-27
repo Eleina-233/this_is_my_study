373f3be6eb03c3cf7d78a87752463901954a2709
Eleina-233 <76209763+Eleina-233@users.noreply.github.com>
2022-04-27 10:02:38 +0800

    Delete java代码学习/臭宝 directory

--- java代码学习/臭宝/javatest/src/main/java/upload/thread/TestPool.java
+++
@@ -1,9 +0,0 @@
-package upload.thread;
-
-/**
- * Created by upload on 2021/11/4.
- */
-public class TestPool {
-
-
-}
--- java代码学习/臭宝/javaweb/javaweb/servlet-01-test/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello World!1111111111111w_l_scy</h2>
-</body>
-</html>
